0e083ec7c67c0afcf3d6c40e5fa22712d819cad4
Recher <recher@plouf.com>
2020-01-10 00:47:19 +0100

    Correction lecture des blocs de données LBM. Y'a pas de chunk!

--- bazar/scripts_python_helpers/lbm_img_tools.py
+++ bazar/scripts_python_helpers/lbm_img_tools.py
@@ -1,43 +1,43 @@
 from create_bmp_file import Bitmap
 
 
-def color_indexes_from_lbm_chunk(bytes_lbm_chunks, cursor=0, verbose=True):
+def color_indexes_from_lbm_chunk(bytes_lbm_block, cursor=0, verbose=True):
     """
-    Un chunk est constitué d'un ou plusieurs blocs.
-    Les blocs ensemble sont constitués de parts.
+    Blabla.
+    Les 4 premiers octets indiquent la taille du bloc.
+    Et ensuite on a tout le bloc.
     """
-    merged_block_data = []
+    block_data = []
     cursor = 0
-    block_size = None
-    block_index = 0
-    while cursor < len(bytes_lbm_chunks) and (block_size is None or block_size == 3768):
+    block_size = list(map(int, bytes_lbm_block[cursor:cursor+4]))
+    cursor += 4
 
-        block_size = list(map(int, bytes_lbm_chunks[cursor:cursor+4]))
-        cursor += 4
+    block_size = sum([
+        byte*(256**offset)
+        for byte, offset
+        in zip(block_size, range(4))
+    ])
 
-        block_size = sum([
-            byte*(256**offset)
-            for byte, offset
-            in zip(block_size, range(4))
-        ])
+    if verbose:
+        print("Taille du bloc : %s." % block_size)
 
-        if verbose:
-            print("Block numéro %s. Taille : %s" % (block_index, block_size))
+    block_data += list(map(int, bytes_lbm_block[cursor:cursor+block_size]))
+    cursor += block_size
 
-        merged_block_data += list(map(int, bytes_lbm_chunks[cursor:cursor+block_size]))
-        cursor += block_size
-        block_index += 1
+    if cursor != len(bytes_lbm_block):
+        msg = "Taille de bloc indiqué (%s) différente de la taille de bloc réelle (%s)."
+        raise Exception(msg % (cursor, len(bytes_lbm_block)))
 
     color_indexes = []
-    while merged_block_data:
-        part_header = merged_block_data.pop(0)
+    while block_data:
+        part_header = block_data.pop(0)
         if part_header > 128:
-            part_body = merged_block_data.pop(0)
+            part_body = block_data.pop(0)
             color_indexes += [ part_body ] * (257-part_header)
         else:
             body_size = part_header + 1
-            part_body = merged_block_data[:body_size]
-            merged_block_data = merged_block_data[body_size:]
+            part_body = block_data[:body_size]
+            block_data = block_data[body_size:]
             color_indexes += part_body
 
     if verbose:
@@ -48,11 +48,11 @@ def color_indexes_from_lbm_chunk(bytes_lbm_chunks, cursor=0, verbose=True):
     return color_indexes
 
 
-def palette_from_lbm_data(bytes_lbm_chunks, cursor=0, verbose=True):
+def palette_from_lbm_data(bytes_lbm_block, cursor=0, verbose=True):
 
-    if len(bytes_lbm_chunks) != 768:
+    if len(bytes_lbm_block) != 768:
         raise ValueError("La taille du chunk de palette doit être de 768 octets")
-    bytes_palette = list(map(int, bytes_lbm_chunks))
+    bytes_palette = list(map(int, bytes_lbm_block))
     if verbose:
         print("Valeur max des RVB de la palette : %s" % max(bytes_palette))
 
